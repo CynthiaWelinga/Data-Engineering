098b44d3010e033bf7c7046569789aa2b220ec7c
Cynthia Welinga <42123588+CynthiaWelinga@users.noreply.github.com>
2021-01-19 20:14:45 -0500

    Update main.py

--- Retrieve Your Spotify User Data/main.py
+++ Retrieve Your Spotify User Data/main.py
@@ -7,8 +7,6 @@ from datetime import datetime
 import datetime 
 import sqlite3
 
-# Extraction
-
 DATABASE_LOCATION = "sqlite://my_played_tracks.sqlite"
 USER_ID = "22hkkqxsnuncuzcgmwimr5mma"
 TOKEN = "BQCM_Fnc2NScw2uAJ3XyWL83IJSgTf3C5ExZRrV2Iidc53jB5FFyOs4AdwC1ml9c0uacywvGsEk3GCRWTGq5SA23Q7Oo3h7aD68irrEGdUC7bkp8_TjYMH-K-6_AqUPJ6u5er3CKYr2PFkptRbgnB23nU4YKOtZ2wG64CclS3k52QCKMsd-BLeA"
@@ -20,10 +18,10 @@ if __name__ == "__main__":
     }
 
     today = datetime.datetime.now()
-    yesterday = today - datetime.timedelta(days = 1)
+    yesterday = today - datetime.timedelta(days = 60)
     yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
 
     req = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time=yesterday_unix_timestamp), headers = headers)
     data = req.json()
-    #data = requests.get(url).json()
+   
     print(data)
