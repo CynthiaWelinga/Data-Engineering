ca92aeaa11c23af9abcb32975783d61f8d2ac92c
Cynthia Welinga <42123588+CynthiaWelinga@users.noreply.github.com>
2021-03-30 11:40:44 -0400

    Update app.py

--- Extract Twitter Data/app.py
+++ Extract Twitter Data/app.py
@@ -1,22 +1,20 @@
-import tweepy #pip3 install tweepy
+import tweepy # pip3 install tweepy
 
 # Create twitter account, if you don't have one
 # Generate API & Access tokens from the link below
 # https://developer.twitter.com/, Navigate to Apps and app name to generate all keys & secrets
 
-API_KEY = 'A7BddVHNBz60sNd0Ggp83ZXiX'
-API_SECRET = 'DAsp6a6bZTdlSjGYNSAjXs03GvuRlUaU95pvG8teds4M4JmHcW'
-ACCESS_TOKEN = '3246071179-XRSqSHp2akfcm0ntTtp9L6BI18LWffDSujHtqkM'
-ACCESS_TOKEN_SECRET = 'Q6bi2aTkksps8OicljdZ8yTzwnKfCGE6Fin46v0PtoNQg'
+API_KEY = 'bwEFzTsrqIQswrOahG5lUASeN'
+API_SECRET = 'jZSM1qJfUvIKtctV6hOfdnWqpioGq3tCZt20dbsylZSaeK67ns'
+ACCESS_TOKEN = '1189692749127258112-r7GpYp1ZXrSj0qip5vHPU2UrTdbuYG'
+ACCESS_TOKEN_SECRET = '0Mepu7yIenuT79KboAY35sHaBvwTWSyYDVrlMPQd0PXfv'
 
 # Authenticate and make sure your keys work
 auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 # Create API object
-api = tweepy.API(auth, wait_on_rate_limit=True,
-    wait_on_rate_limit_notify=True)
-# api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 try:
     api.verify_credentials()
@@ -25,7 +23,7 @@ except:
     print("Error during authentication")
 
 # Check for latest 100 tweets with hashtag python or orskl
-tweets = api.search(q="Python", lang="en", rpp=100)
+tweets = api.search(q="International Women's Day", lang="en", rpp=100)
 for tweet in tweets:
     USER = tweet.user.name
     RETWEETS = tweet.retweet_count
@@ -38,7 +36,7 @@ for tweet in tweets:
 # Create a class that listens streaming tweets with hashtag python or orskl
 # Refer to https://docs.python.org/3/tutorial/classes.html for details on Python classes
 
-class OrSklStream(tweepy.StreamListener):
+class Tweet_Stream(tweepy.StreamListener):
     # Class inheritance as we discussed in Track 1 Intro session
     # Inherting tweepy.StreamListener class in to OrSklStream class
     # More info on inheritances - https://docs.python.org/3/tutorial/classes.html#inheritance
@@ -54,14 +52,15 @@ class OrSklStream(tweepy.StreamListener):
         CREATED = tweet.created_at
         TEXT = tweet.text
         SOURCE = tweet.source
-        print(USER + " | " + TEXT + " | " + str(CREATED) + " | " + str(RETWEETS) + " | " + SOURCE)
-        file = open('./Track 5 | Python Data Engineering | Streaming data | Extract & Transform/tweets_extract.csv', "a")
+        #print(USER + " | " + TEXT + " | " + str(CREATED) + " | " + str(RETWEETS) + " | " + SOURCE)
+        tfile = open("C:/Users/cynthiajuma/Desktop\PROGRAMMING/DATA_ENGINEERING/Twitter_Project/tweets_extract.csv", "a")
         output = USER + " | " + TEXT + " | " + str(CREATED) + " | " + str(RETWEETS) + " | " + SOURCE + '\n'
-        file.write(output)
-        file.close()
+        tfile.write(output)
+        tfile.close()
+
 
 # Let this run in PyCharm and test
-tweets_listener = OrSklStream(api)
+tweets_listener = Tweet_Stream(api)
 stream = tweepy.Stream(api.auth, tweets_listener)
 stream.filter(track=["Python", "Orskl"], languages=["en"])
 
